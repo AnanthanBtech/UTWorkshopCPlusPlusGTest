7bb8810ebc9bbe36cfc8b62ec6dc909c4957820f
apoongavanam <Ananthan.Poongavanam@harman.com>
2020-06-08 11:35:24 +0530

    combine the all the test cases for generate the coverage

---
+++ build/CMakeFiles/runTests.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/data/home/apoongavanam/UTWorkShop/HarmanUniversity/UTWorkshopCPlusPlusGTest/src/LeapYear.cpp" "/data/home/apoongavanam/UTWorkShop/HarmanUniversity/UTWorkshopCPlusPlusGTest/build/CMakeFiles/runTests.dir/src/LeapYear.cpp.o"
+  "/data/home/apoongavanam/UTWorkShop/HarmanUniversity/UTWorkshopCPlusPlusGTest/test/LeapYearStandaloneTests.cpp" "/data/home/apoongavanam/UTWorkShop/HarmanUniversity/UTWorkshopCPlusPlusGTest/build/CMakeFiles/runTests.dir/test/LeapYearStandaloneTests.cpp.o"
+  "/data/home/apoongavanam/UTWorkShop/HarmanUniversity/UTWorkshopCPlusPlusGTest/test/LeapYearStandaloneTests_Suresh.cpp" "/data/home/apoongavanam/UTWorkShop/HarmanUniversity/UTWorkshopCPlusPlusGTest/build/CMakeFiles/runTests.dir/test/LeapYearStandaloneTests_Suresh.cpp.o"
+  "/data/home/apoongavanam/UTWorkShop/HarmanUniversity/UTWorkshopCPlusPlusGTest/test/main.cpp" "/data/home/apoongavanam/UTWorkShop/HarmanUniversity/UTWorkshopCPlusPlusGTest/build/CMakeFiles/runTests.dir/test/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "test"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /data/home/apoongavanam/UTWorkShop/HarmanUniversity/UTWorkshopCPlusPlusGTest
+# Build directory: /data/home/apoongavanam/UTWorkShop/HarmanUniversity/UTWorkshopCPlusPlusGTest/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(runTests "/data/home/apoongavanam/UTWorkShop/HarmanUniversity/UTWorkshopCPlusPlusGTest/build/runTests")
